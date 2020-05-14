f3c58dbb3da0451d8c419ecc2c3c0fff1fb8e58f
basilkaffi <basilarr7@gmail.com>
2020-05-14 18:01:35 +0700

    adding socket to server

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const port = process.env.PORT
 const app = express()
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.use(cors())
 app.use(express.json())
@@ -12,8 +14,25 @@ app.use(express.urlencoded({extended: true}))
 app.use(routes)
 app.use(errorHandler)
 
-app.listen(port, () => {
-  console.log(`listening on ${port}`)
+io.on('connection', socket => {
+  console.log('user connected')
+  socket.on('attack', element => {
+      socket.broadcast.emit('enemy attacking', element)
+  })
+  socket.on('nyawa', nyawa => {
+      socket.broadcast.emit('nyawa musuh', nyawa)
+  })
+
+  socket.on('disconnect', () => {
+      console.log('user disconnected')
+  })
 })
+
+http.listen(port, () => {
+  console.log('listen to port '+port)
+})
+// app.listen(port, () => {
+//   console.log(`listening on ${port}`)
+// })
 
 module.exports = app
